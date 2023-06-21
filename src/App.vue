9e68e9745a1cf3a21d607583059feebc244a2736
Maxim Lapis <maxim.lapis@gmx.de>
2023-06-21 11:55:42 +0200

    rename

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <HelloWorld />
+  <AbContentBlocks />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import AbContentBlocks from "./components/AbContentBlocks.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    AbContentBlocks,
   },
 });
 </script>
